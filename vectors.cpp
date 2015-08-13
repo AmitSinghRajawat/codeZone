4b31b3223152c8d22810686063e0a7749883f522
Amit Rajawat <avi.05raj@gmail.com>
2015-08-13 14:34:55 +0530

    referenceCodes

---
+++ vectors.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int> num;
+	vector<string> str;
+	string name;
+	int i;
+	string  w[10];
+	for(i=0;i<10;i++){
+		num.push_back(i);
+	}
+//	getline(cin,name);
+i=0;
+//	while(i++<3){
+//		getline(cin,name);
+//	str.push_back(name);
+//	}
+char name1 []="this is my name";
+char *p;
+p = strtok(name," ");
+cout<<str.back();
+	for(i=0;i<str.size();i++){
+	//	cout<<str[i].size()<<endl;
+	}
+//	name = "2011";
+//	i=stoi(name);
+//	cout<<i;
+}
